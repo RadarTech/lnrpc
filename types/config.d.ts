2e04a0a9aed9b8a38a39f8e6e05a672470d4081b
c t <caleb+github@tebbe.io>
2019-01-09 16:51:50 -0700

    refactor config vars to object

--- types/config.d.ts
+++ types/config.d.ts
@@ -1,5 +1,4 @@
 import { GrpcObject } from "grpc";
-import { LnRpc } from "./lnrpc";
 
 export interface LnRpcClientConfig {
   server: string; // URL for the lightning node to connect to ie. localhost:10009
@@ -7,8 +6,8 @@ export interface LnRpcClientConfig {
   cert: string; // string representation of tls.cert
   macaroonPath: string;
   macaroon: string; // hex-encoded string of macaroon file
-  lightning: LnRpc.lightning;
-  walletUnlocker: LnRpc.walletUnlocker;
+  lightning: object;
+  walletUnlocker: object;
   grpcLoader: any;
   grpc: GrpcObject;
 }
