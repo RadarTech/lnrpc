092b473fccf67bacc1b7898ea14c7235b1658c53
c t <caleb+github@tebbe.io>
2019-01-09 16:26:05 -0700

    add missing LnRpcClientConfig vars

--- types/config.d.ts
+++ types/config.d.ts
@@ -1,7 +1,14 @@
+import { GrpcObject } from "grpc";
+import { LnRpc } from "./lnrpc";
+
 export interface LnRpcClientConfig {
   server: string; // URL for the lightning node to connect to ie. localhost:10009
   tls: string; // /path/to/tls.cert
   cert: string; // string representation of tls.cert
   macaroonPath: string;
   macaroon: string; // hex-encoded string of macaroon file
+  lightning: LnRpc.lightning;
+  walletUnlocker: LnRpc.walletUnlocker;
+  grpcLoader: any;
+  grpc: GrpcObject;
 }
