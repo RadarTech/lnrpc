c967fc38712c5c4f931fd43b58fcd85e05feb54e
Cavan <cavan@radarrelay.com>
2019-01-30 17:41:02 -0700

    Correct parameter types

--- types/lnrpc.d.ts
+++ types/lnrpc.d.ts
@@ -1,131 +1,155 @@
-import {
-  AbandonChannelRequest,
-  AbandonChannelResponse,
-  AddInvoiceResponse,
-  ChangePasswordRequest,
-  ChangePasswordResponse,
-  ChanInfoRequest,
-  ChannelBalanceRequest,
-  ChannelBalanceResponse,
-  ChannelEdge,
-  ChannelGraph,
-  ChannelGraphRequest,
-  ChannelPoint,
-  ClosedChannelsRequest,
-  ClosedChannelsResponse,
-  ConnectPeerRequest,
-  ConnectPeerResponse,
-  DebugLevelRequest,
-  DebugLevelResponse,
-  DeleteAllPaymentsRequest,
-  DeleteAllPaymentsResponse,
-  DisconnectPeerRequest,
-  DisconnectPeerResponse,
-  FeeReportRequest,
-  FeeReportResponse,
-  ForwardingHistoryRequest,
-  ForwardingHistoryResponse,
-  GenSeedRequest,
-  GenSeedResponse,
-  GetInfoRequest,
-  GetInfoResponse,
-  GetTransactionsRequest,
-  GraphTopologySubscription,
-  GraphTopologyUpdate,
-  InitWalletRequest,
-  InitWalletResponse,
-  Invoice,
-  InvoiceSubscription,
-  ListChannelsRequest,
-  ListChannelsResponse,
-  ListInvoiceRequest,
-  ListInvoiceResponse,
-  ListPaymentsRequest,
-  ListPaymentsResponse,
-  ListPeersRequest,
-  ListPeersResponse,
-  NetworkInfo,
-  NetworkInfoRequest,
-  NewAddressRequest,
-  NewAddressResponse,
-  NodeInfo,
-  NodeInfoRequest,
-  OpenChannelRequest,
-  OpenStatusUpdate,
-  PaymentHash,
-  PayReq,
-  PayReqString,
-  PendingChannelsRequest,
-  PendingChannelsResponse,
-  PolicyUpdateRequest,
-  PolicyUpdateResponse,
-  QueryRoutesRequest,
-  QueryRoutesResponse,
-  SendCoinsRequest,
-  SendCoinsResponse,
-  SendManyRequest,
-  SendManyResponse,
-  SendRequest,
-  SendResponse,
-  SendToRouteRequest,
-  SignMessageRequest,
-  SignMessageResponse,
-  StopRequest,
-  StopResponse,
-  Transaction,
-  TransactionDetails,
-  UnlockWalletRequest,
-  UnlockWalletResponse,
-  VerifyMessageRequest,
-  VerifyMessageResponse,
-  WalletBalanceRequest,
-  WalletBalanceResponse,
-} from "./generated/rpc_pb";
+import Gen from './generated/rpc_pb';
+import { Stream } from 'stream';
+
+// Overwrite specific properties
+type Overwrite<T1, T2> = {
+  [P in Exclude<keyof T1, keyof T2>]: T1[P]
+} & T2;
+
+export type GenSeedRequest = Partial<Gen.GenSeedRequest.AsObject>;
+export type GenSeedResponse = Gen.GenSeedResponse.AsObject;
+export type InitWalletRequest = Overwrite<Gen.InitWalletRequest.AsObject, {
+  aezeedPassphrase?: Uint8Array | string,
+  recoveryWindow?: number,
+}>;
+export type InitWalletResponse = Gen.InitWalletResponse.AsObject;
+export type UnlockWalletRequest = Overwrite<Gen.UnlockWalletRequest.AsObject, {
+  recoveryWindow?: number,
+}>;
+export type UnlockWalletResponse = Gen.UnlockWalletResponse.AsObject;
+export type ChangePasswordRequest = Gen.ChangePasswordRequest.AsObject;
+export type ChangePasswordResponse = Gen.ChangePasswordResponse.AsObject;
+export type WalletBalanceRequest = Gen.WalletBalanceRequest.AsObject;
+export type WalletBalanceResponse = Gen.WalletBalanceResponse.AsObject;
+export type ChannelBalanceRequest = Gen.ChannelBalanceRequest.AsObject;
+export type ChannelBalanceResponse = Gen.ChannelBalanceResponse.AsObject;
+export type GetTransactionsRequest = Gen.GetTransactionsRequest.AsObject;
+export type TransactionDetails = Gen.TransactionDetails.AsObject;
+export type SendCoinsRequest = Overwrite<Gen.SendCoinsRequest.AsObject, {
+  targetConf?: number,
+  satPerByte?: string,
+}>;
+export type SendCoinsResponse = Gen.SendCoinsResponse.AsObject;
+export type SendManyRequest = Overwrite<Gen.SendManyRequest.AsObject, {
+  targetConf?: number,
+  satPerByte?: string,
+}>;
+export type SendManyResponse = Gen.SendManyResponse.AsObject;
+export type NewAddressRequest = Partial<Gen.NewAddressRequest.AsObject>;
+export type NewAddressResponse = Gen.NewAddressResponse.AsObject;
+export type SignMessageRequest = Gen.SignMessageRequest.AsObject;
+export type SignMessageResponse = Gen.SignMessageResponse.AsObject;
+export type VerifyMessageRequest = Gen.VerifyMessageRequest.AsObject;
+export type VerifyMessageResponse = Gen.VerifyMessageResponse.AsObject;
+export type ConnectPeerRequest = Overwrite<Gen.ConnectPeerRequest.AsObject, {
+  perm?: boolean,
+}>;
+export type ConnectPeerResponse = Gen.ConnectPeerResponse.AsObject;
+export type DisconnectPeerRequest = Gen.DisconnectPeerRequest.AsObject;
+export type DisconnectPeerResponse = Gen.DisconnectPeerResponse.AsObject;
+export type ListPeersRequest = Gen.ListPeersRequest.AsObject;
+export type ListPeersResponse = Gen.ListPeersResponse.AsObject;
+export type GetInfoRequest = Gen.GetInfoRequest.AsObject;
+export type GetInfoResponse = Gen.GetInfoResponse.AsObject;
+export type PendingChannelsRequest = Gen.PendingChannelsRequest.AsObject;
+export type PendingChannelsResponse = Gen.PendingChannelsResponse.AsObject;
+export type ListChannelsRequest = Partial<Gen.ListChannelsRequest.AsObject>;
+export type ListChannelsResponse = Gen.ListChannelsResponse.AsObject;
+export type ClosedChannelsRequest = Partial<Gen.ClosedChannelsRequest.AsObject>;
+export type ClosedChannelsResponse = Gen.ClosedChannelsResponse.AsObject;
+export type OpenChannelRequest = Partial<Gen.OpenChannelRequest.AsObject>;
+export type ChannelPoint = Gen.ChannelPoint.AsObject;
+export type AbandonChannelRequest = Gen.AbandonChannelRequest.AsObject;
+export type AbandonChannelResponse = Gen.AbandonChannelResponse.AsObject;
+export type SendRequest = Partial<Gen.SendRequest.AsObject>;
+export type SendResponse = Gen.SendResponse.AsObject;
+export type SendToRouteRequest = Partial<Gen.SendToRouteRequest.AsObject>;
+export type Invoice = Partial<Gen.Invoice.AsObject>;
+export type AddInvoiceResponse = Gen.AddInvoiceResponse.AsObject;
+export type ListInvoiceRequest = Partial<Gen.ListInvoiceRequest.AsObject>;
+export type ListInvoiceResponse = Gen.ListInvoiceResponse.AsObject;
+export type PaymentHash = Partial<Gen.PaymentHash.AsObject>;
+export type InvoiceSubscription = Gen.InvoiceSubscription.AsObject;
+export type PayReqString = Gen.PayReqString.AsObject;
+export type PayReq = Gen.PayReq.AsObject;
+export type ListPaymentsRequest = Gen.ListPaymentsRequest.AsObject;
+export type ListPaymentsResponse = Gen.ListPaymentsResponse.AsObject;
+export type DeleteAllPaymentsRequest = Gen.DeleteAllPaymentsRequest.AsObject;
+export type DeleteAllPaymentsResponse = Gen.DeleteAllPaymentsResponse.AsObject;
+export type ChannelGraphRequest = Partial<Gen.ChannelGraphRequest.AsObject>;
+export type ChannelGraph = Gen.ChannelGraph.AsObject;
+export type ChanInfoRequest = Gen.ChanInfoRequest.AsObject;
+export type ChannelEdge = Gen.ChannelEdge.AsObject;
+export type NodeInfoRequest = Gen.NodeInfoRequest.AsObject;
+export type NodeInfo = Gen.NodeInfo.AsObject;
+export type QueryRoutesRequest = Overwrite<Gen.QueryRoutesRequest.AsObject, {
+  amt?: string,
+  numRoutes?: number,
+  finalCltvDelta?: number,
+}>;
+export type QueryRoutesResponse = Gen.QueryRoutesResponse.AsObject;
+export type NetworkInfoRequest = Gen.NetworkInfoRequest.AsObject;
+export type NetworkInfo = Gen.NetworkInfo.AsObject;
+export type StopRequest = Gen.StopRequest.AsObject;
+export type StopResponse = Gen.StopResponse.AsObject;
+export type GraphTopologySubscription = Gen.GraphTopologySubscription.AsObject;
+export type DebugLevelRequest = Overwrite<Gen.DebugLevelRequest.AsObject, {
+  show?: boolean,
+}>;
+export type DebugLevelResponse = Gen.DebugLevelResponse.AsObject;
+export type FeeReportRequest = Gen.FeeReportRequest.AsObject;
+export type FeeReportResponse = Gen.FeeReportResponse.AsObject;
+export type PolicyUpdateRequest = Overwrite<Gen.PolicyUpdateRequest.AsObject, {
+  global?: boolean,
+}>;
+export type PolicyUpdateResponse = Gen.PolicyUpdateResponse.AsObject;
+export type ForwardingHistoryRequest = Partial<Gen.ForwardingHistoryRequest.AsObject>;
+export type ForwardingHistoryResponse = Gen.ForwardingHistoryResponse.AsObject;
 
 export class LnRpc {
-  public genSeed(args: GenSeedRequest.AsObject): Promise<GenSeedResponse.AsObject>;
-  public initWallet(args: InitWalletRequest.AsObject): Promise<InitWalletResponse.AsObject>;
-  public unlockWallet(args: UnlockWalletRequest.AsObject): Promise<UnlockWalletResponse.AsObject>;
-  public changePassword(args: ChangePasswordRequest.AsObject): Promise<ChangePasswordResponse.AsObject>;
-  public walletBalance(args: WalletBalanceRequest.AsObject): Promise<WalletBalanceResponse.AsObject>;
-  public channelBalance(args: ChannelBalanceRequest.AsObject): Promise<ChannelBalanceResponse.AsObject>;
-  public getTransactions(args: GetTransactionsRequest.AsObject): Promise<TransactionDetails.AsObject>;
-  public sendCoins(args: SendCoinsRequest.AsObject): Promise<SendCoinsResponse.AsObject>;
-  public subscribeTransactions(args: GetTransactionsRequest.AsObject): Promise<Transaction.AsObject>;
-  public sendMany(args: SendManyRequest.AsObject): Promise<SendManyResponse.AsObject>;
-  public newAddress(args: NewAddressRequest.AsObject): Promise<NewAddressResponse.AsObject>;
-  public signMessage(args: SignMessageRequest.AsObject): Promise<SignMessageResponse.AsObject>;
-  public verifyMessage(args: VerifyMessageRequest.AsObject): Promise<VerifyMessageResponse.AsObject>;
-  public connectPeer(args: ConnectPeerRequest.AsObject): Promise<ConnectPeerResponse.AsObject>;
-  public disconnectPeer(args: DisconnectPeerRequest.AsObject): Promise<DisconnectPeerResponse.AsObject>;
-  public listPeers(args: ListChannelsRequest.AsObject): Promise<ListPeersResponse.AsObject>;
-  public getInfo(args: GetInfoRequest.AsObject): Promise<GetInfoResponse.AsObject>;
-  public pendingChannels(args: PendingChannelsRequest.AsObject): Promise<PendingChannelsResponse.AsObject>;
-  public listChannels(args: ListChannelsRequest.AsObject): Promise<ListChannelsResponse.AsObject>;
-  public closedChannels(args: ClosedChannelsRequest.AsObject): Promise<ClosedChannelsResponse.AsObject>;
-  public openChannelSync(args: OpenChannelRequest.AsObject): Promise<ChannelPoint.AsObject>;
-  public openChannel(args: OpenChannelRequest.AsObject): Promise<OpenStatusUpdate.AsObject>;
-  public abandonChannel(args: AbandonChannelRequest.AsObject): Promise<AbandonChannelResponse.AsObject>;
-  public sendPayment(args: SendRequest.AsObject): Promise<SendResponse.AsObject>;
-  public sendPaymentSync(args: SendRequest.AsObject): Promise<SendResponse.AsObject>;
-  public sendToRoute(args: SendToRouteRequest.AsObject): Promise<SendResponse.AsObject>;
-  public sendToRouteSync(args: SendToRouteRequest.AsObject): Promise<SendResponse.AsObject>;
-  public addInvoice(args: Invoice.AsObject): Promise<AddInvoiceResponse.AsObject>;
-  public listInvoices(args: ListInvoiceRequest.AsObject): Promise<ListInvoiceResponse.AsObject>;
-  public lookupInvoice(args: PaymentHash.AsObject): Promise<Invoice.AsObject>;
-  public subscribeInvoices(args: InvoiceSubscription.AsObject): Promise<Invoice.AsObject>;
-  public decodePayReq(args: PayReqString.AsObject): Promise<PayReq.AsObject>;
-  public listPayments(args: ListChannelsRequest.AsObject): Promise<ListPaymentsResponse.AsObject>;
-  public deleteAllPayments(args: DeleteAllPaymentsRequest.AsObject): Promise<DeleteAllPaymentsResponse.AsObject>;
-  public describeGraph(args: ChannelGraphRequest.AsObject): Promise<ChannelGraph.AsObject>;
-  public getChanInfo(args: ChanInfoRequest.AsObject): Promise<ChannelEdge.AsObject>;
-  public getNodeInfo(args: NodeInfoRequest.AsObject): Promise<NodeInfo.AsObject>;
-  public queryRoutes(args: QueryRoutesRequest.AsObject): Promise<QueryRoutesResponse.AsObject>;
-  public getNetworkInfo(args: NetworkInfoRequest.AsObject): Promise<NetworkInfo.AsObject>;
-  public stopDaemon(args: StopResponse.AsObject): Promise<StopResponse.AsObject>;
-  public subscribeChannelGraph(args: GraphTopologySubscription.AsObject): Promise<GraphTopologyUpdate.AsObject>;
-  public debugLevel(args: DebugLevelRequest.AsObject): Promise<DebugLevelResponse.AsObject>;
-  public feeReport(args: FeeReportRequest.AsObject): Promise<FeeReportResponse.AsObject>;
-  public updateChannelPolicy(args: PolicyUpdateRequest.AsObject): Promise<PolicyUpdateResponse.AsObject>;
-  public forwardingHistory(args: ForwardingHistoryRequest.AsObject): Promise<ForwardingHistoryResponse.AsObject>;
+  public genSeed(args: GenSeedRequest): Promise<GenSeedResponse>;
+  public initWallet(args: InitWalletRequest): Promise<InitWalletResponse>;
+  public unlockWallet(args: UnlockWalletRequest): Promise<UnlockWalletResponse>;
+  public changePassword(args: ChangePasswordRequest): Promise<ChangePasswordResponse>;
+  public walletBalance(args: WalletBalanceRequest): Promise<WalletBalanceResponse>;
+  public channelBalance(args: ChannelBalanceRequest): Promise<ChannelBalanceResponse>;
+  public getTransactions(args: GetTransactionsRequest): Promise<TransactionDetails>;
+  public sendCoins(args: SendCoinsRequest): Promise<SendCoinsResponse>;
+  public subscribeTransactions(args: GetTransactionsRequest): Promise<Stream>;
+  public sendMany(args: SendManyRequest): Promise<SendManyResponse>;
+  public newAddress(args: NewAddressRequest): Promise<NewAddressResponse>;
+  public signMessage(args: SignMessageRequest): Promise<SignMessageResponse>;
+  public verifyMessage(args: VerifyMessageRequest): Promise<VerifyMessageResponse>;
+  public connectPeer(args: ConnectPeerRequest): Promise<ConnectPeerResponse>;
+  public disconnectPeer(args: DisconnectPeerRequest): Promise<DisconnectPeerResponse>;
+  public listPeers(args: ListPeersRequest): Promise<ListPeersResponse>;
+  public getInfo(args: GetInfoRequest): Promise<GetInfoResponse>;
+  public pendingChannels(args: PendingChannelsRequest): Promise<PendingChannelsResponse>;
+  public listChannels(args: ListChannelsRequest): Promise<ListChannelsResponse>;
+  public closedChannels(args: ClosedChannelsRequest): Promise<ClosedChannelsResponse>;
+  public openChannelSync(args: OpenChannelRequest): Promise<ChannelPoint>;
+  public openChannel(args: OpenChannelRequest): Promise<Stream>;
+  public abandonChannel(args: AbandonChannelRequest): Promise<AbandonChannelResponse>;
+  public sendPayment(args: SendRequest): Promise<Stream>;
+  public sendPaymentSync(args: SendRequest): Promise<SendResponse>;
+  public sendToRoute(args: SendToRouteRequest): Promise<Stream>;
+  public sendToRouteSync(args: SendToRouteRequest): Promise<SendResponse>;
+  public addInvoice(args: Invoice): Promise<AddInvoiceResponse>;
+  public listInvoices(args: ListInvoiceRequest): Promise<ListInvoiceResponse>;
+  public lookupInvoice(args: PaymentHash): Promise<Invoice>;
+  public subscribeInvoices(args: InvoiceSubscription): Promise<Stream>;
+  public decodePayReq(args: PayReqString): Promise<PayReq>;
+  public listPayments(args: ListPaymentsRequest): Promise<ListPaymentsResponse>;
+  public deleteAllPayments(args: DeleteAllPaymentsRequest): Promise<DeleteAllPaymentsResponse>;
+  public describeGraph(args: ChannelGraphRequest): Promise<ChannelGraph>;
+  public getChanInfo(args: ChanInfoRequest): Promise<ChannelEdge>;
+  public getNodeInfo(args: NodeInfoRequest): Promise<NodeInfo>;
+  public queryRoutes(args: QueryRoutesRequest): Promise<QueryRoutesResponse>;
+  public getNetworkInfo(args: NetworkInfoRequest): Promise<NetworkInfo>;
+  public stopDaemon(args: StopRequest): Promise<StopResponse>;
+  public subscribeChannelGraph(args: GraphTopologySubscription): Promise<Stream>;
+  public debugLevel(args: DebugLevelRequest): Promise<DebugLevelResponse>;
+  public feeReport(args: FeeReportRequest): Promise<FeeReportResponse>;
+  public updateChannelPolicy(args: PolicyUpdateRequest): Promise<PolicyUpdateResponse>;
+  public forwardingHistory(args: ForwardingHistoryRequest): Promise<ForwardingHistoryResponse>;
 }
