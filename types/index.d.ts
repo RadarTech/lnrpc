4e0f8052802d2d384e1ba0046269beef55a0267a
Cavan <cavan@radarrelay.com>
2019-03-04 18:06:37 -0700

    Remove default export from typings

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,7 +1,10 @@
 import { LnRpcClientConfig } from "./config";
 import { LnRpc } from "./lnrpc";
 
-export * from "./config";
-export * from "./lnrpc";
-
-export default function createLnRpc(config: LnRpcClientConfig): Promise<LnRpc>;
+declare module "@radar/lnrpc" {
+  export * from "./config";
+  export * from "./lnrpc";
+  
+  function createLnRpc(config: LnRpcClientConfig): Promise<LnRpc>;
+  export = createLnRpc;
+}
