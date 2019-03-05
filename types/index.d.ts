0240cfd41fb40b606f0658fcb0764f6d2becd7c6
Cavan <cavan@radarrelay.com>
2019-03-04 18:08:15 -0700

    Remove trailing whitespace

--- types/index.d.ts
+++ types/index.d.ts
@@ -4,7 +4,7 @@ import { LnRpc } from "./lnrpc";
 declare module "@radar/lnrpc" {
   export * from "./config";
   export * from "./lnrpc";
-  
+
   function createLnRpc(config: LnRpcClientConfig): Promise<LnRpc>;
   export = createLnRpc;
 }
